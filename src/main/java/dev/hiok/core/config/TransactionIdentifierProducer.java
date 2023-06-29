26bd7d9fc4de8c2592f0b62f01e8d8826bfb8f89
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-06-29 02:08:37 -0300

    add transaction identifier

---
+++ src/main/java/dev/hiok/core/config/TransactionIdentifierProducer.java
@@ -0,0 +1,25 @@
+package dev.hiok.core.config;
+
+import dev.hiok.core.utils.TransactionIdentifier;
+import io.quarkus.arc.DefaultBean;
+import io.quarkus.arc.profile.IfBuildProfile;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.inject.Produces;
+
+public class TransactionIdentifierProducer {
+
+  @Produces
+  @RequestScoped
+  @DefaultBean
+  public TransactionIdentifier testProduce() {
+    return new TransactionIdentifier("Test:");
+  }
+
+  @Produces
+  @RequestScoped
+  @IfBuildProfile("prod")
+  public TransactionIdentifier prodProduce() {
+    return new TransactionIdentifier("Prod:");
+  }
+
+}
--- src/main/java/dev/hiok/domain/service/FruitService.java
+++ src/main/java/dev/hiok/domain/service/FruitService.java
@@ -1,10 +1,12 @@
 package dev.hiok.domain.service;
 
+import dev.hiok.core.utils.TransactionIdentifier;
 import dev.hiok.domain.entity.FruitEntity;
 import io.quarkus.hibernate.reactive.panache.Panache;
 import io.quarkus.hibernate.reactive.panache.common.WithSession;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 
 import java.util.List;
 
@@ -12,15 +14,21 @@ import java.util.List;
 @ApplicationScoped
 public class FruitService {
 
+  @Inject
+  TransactionIdentifier transactionIdentifier;
+
   public Uni<List<FruitEntity>> list() {
+    System.out.println(transactionIdentifier.getTransactionIdentifier());
     return FruitEntity.listAll();
   }
 
   public Uni<FruitEntity> findById(Long id) {
+    System.out.println(transactionIdentifier.getTransactionIdentifier());
     return FruitEntity.findById(id);
   }
 
   public Uni<FruitEntity> create(FruitEntity fruit) {
+    System.out.println(transactionIdentifier.getTransactionIdentifier());
     var newFruit = new FruitEntity();
     newFruit.name = fruit.name;
     newFruit.quantity = fruit.quantity;
